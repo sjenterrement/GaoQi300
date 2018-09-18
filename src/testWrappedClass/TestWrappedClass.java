b574b3676c83678f6ff0c2d0fe23938542e142d8
sjenterrement <sjenterrement@gmail.com>
2018-09-18 18:38:45 +0800

    2018.9.18

---
+++ src/testWrappedClass/TestWrappedClass.java
@@ -0,0 +1,30 @@
+package testWrappedClass;
+
+/**
+ * 测试自动装箱和拆箱
+ * @author savrina
+ *
+ */
+
+public class TestWrappedClass {
+	public static void main(String[] args) {
+		Integer a=new Integer(1000);
+		Integer a1=1000;//jdk5.0,自动装箱，编译器帮我们改变代码
+		
+		int c=new Integer(1111);
+		
+		System.out.println(c);//编译器改进：new Integer（1111）.intValue()
+		
+		Integer d1=1234;
+		Integer d2=1234;
+		System.out.println(d1==d2);
+		System.out.println(d1.equals(d2));
+		
+		System.out.println("##################");
+		
+		Integer s1=100;
+		Integer s2=100;
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+	}
+}
