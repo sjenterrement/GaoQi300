037719d171e9705c1f6cf5a2abaee26f57e12c57
sjenterrement <sjenterrement@gmail.com>
2018-10-28 23:19:53 +0800

    文件拷贝：文件数组输入输出流

---
+++ src/io/Copy.java
@@ -0,0 +1,71 @@
+package io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 文件拷贝：文件数组输入输出流
+ * 1.创建流
+ * 2.选择流
+ * 3.操作
+ * 3。关闭
+ * @author SJ
+ *
+ */
+public class Copy {
+	public static void main(String[] args) {
+		Copy("src\\io\\Copy.java", "copy.txt");
+	}
+	
+	public static void Copy(String srcPath,String destPath) {
+		//1.创建流
+		
+				File src = new File(srcPath);//输入流 源头
+				File dest = new File(destPath);//输出流 目的地
+				
+				//2.选择流
+				InputStream is = null;
+				OutputStream os=null;
+				try {
+					is = new FileInputStream(src);
+					os=new FileOutputStream(dest,true);
+					//3.操作
+					byte[] flush = new byte[1024];// 缓冲容器
+					int len = -1;// 接受长度
+					while ((len = is.read(flush)) != -1) {
+						os.write(flush, 0, len);
+					}
+					os.flush();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					//4.释放资源：分别释放，先打开的后关闭
+					try {
+						if (null!=os) {
+							os.close();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					try {
+						if (null!=is) {
+							is.close();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				
+	}
+}
