15e4ec5260ce852d440a5c3aa15d89cbfb5df1fe
sjenterrement <sjenterrement@gmail.com>
2019-03-19 22:05:52 +0800

    线程方法二：实现runnable接口，创建实现对象

---
+++ src/thread/StartRun.java
@@ -0,0 +1,28 @@
+package thread;
+/**
+ * 实现runnable+重写run
+ * 启动：创建实现类对象+thread对象+start
+ * @author sj
+ *
+ */
+public class StartRun implements Runnable{
+	@Override
+	public void run() {
+		for(int i=0;i<20;i++) {
+			System.out.println("music");
+		}
+	}
+	public static void main(String[] args) {
+//		//创建实现对象
+//		StartRun sr=new StartRun();
+//		//创建代理类对象
+//		Thread t=new Thread(sr);
+//		//启动
+//		t.start();
+		
+		new Thread(new StartRun()).start();
+		
+		for (int i = 0; i < 20; i++)
+			System.out.println("coding");
+	}
+}
