c9ddcf57f1b8966c6030d6a88d0ae31571d166e3
sjenterrement <sjenterrement@gmail.com>
2019-04-08 22:41:32 +0800

    207-->join-1

---
+++ src/thread/BlockJoin01.java
@@ -0,0 +1,17 @@
+package thread;
+
+public class BlockJoin01 {
+	public static void main(String[] args) throws InterruptedException {
+		Thread t = new Thread(()-> {
+			for(int i=0;i<100;i++)
+				System.out.println("lambda.."+i);
+		});
+		t.start();
+		
+		for(int i=0;i<100;i++) {
+			if(i==20)
+				t.join();
+			System.out.println("main...."+i);
+		}
+	}
+}
