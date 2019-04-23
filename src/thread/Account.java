c4f064147bdc99c37ec6225d70df67e8e7aa8084
sjenterrement <sjenterrement@gmail.com>
2019-04-23 19:54:42 +0800

    同步-eg2

---
+++ src/thread/Account.java
@@ -0,0 +1,12 @@
+package thread;
+
+public class Account {
+
+	int money;// ½ð¶î
+	String name;// Ãû³Æ
+
+	public Account(int money, String name) {
+		this.money = money;
+		this.name = name;
+	}
+}
--- src/thread/SynTest02.java
+++ src/thread/SynTest02.java
@@ -1,29 +1,30 @@
 package thread;
 
 /**
- * 线程不安全：取钱
+ * 线程安全：取钱
  * 
  * @author SJ
  *
  */
-public class UnsafeTest02 {
+public class SynTest02 {
 	public static void main(String[] args) {
 		// 账户
 		Account account = new Account(100, "结婚礼金");
-		Drawing you = new Drawing(account, 80, "可悲的你");
-		Drawing wife = new Drawing(account, 90, "happy的她");
+		SafeDrawing you = new SafeDrawing(account, 80, "可悲的你");
+		SafeDrawing wife = new SafeDrawing(account, 90, "happy的她");
 		you.start();
 		wife.start();
 	}
 }
 
+
 // 模拟取款
-class Drawing extends Thread {
+class SafeDrawing extends Thread {
 	Account account;// 取钱的账户
 	int drawingMoney;// 取得钱数
 	int packetTotal;// 取得总数
 
-	public Drawing(Account account, int drawingMoney, String name) {
+	public SafeDrawing(Account account, int drawingMoney, String name) {
 		super(name);
 		this.account = account;
 		this.drawingMoney = drawingMoney;
@@ -31,6 +32,10 @@ class Drawing extends Thread {
 
 	@Override
 	public void run() {
+		test();
+	}
+	
+	public synchronized void test() {
 		if (account.money - drawingMoney < 0) {
 			return;
 		}
